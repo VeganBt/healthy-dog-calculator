20ec40dd795101339e62b63b56e40e4a8128a215
Ofer Peled <ofer2406@gmail.com>
2022-03-23 09:58:50 +0100

    added the option to search for a breed

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -4,26 +4,28 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "results", "form", 'submit', 'suggestions' ]
 
-  connect() {
-    console.log("hello");
-  }
-
   getDogInfo(event) {
     event.preventDefault();
     this.submitTarget.disabled = false;
     const breed = new FormData(this.formTarget).get("breed");
-    const url = `https://api.thedogapi.com/v1/breeds/search?q=${breed}`
-    console.log(url);
-    fetch(url, {
-      method: "GET",
-      headers: {
-                "Accept": "application/json",
-                "X-Api-Key": "f60c3da9-a49e-46f9-8f9c-697e82b80e58"
-              }
-    })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-      });
+    if (breed.length > 2) {
+      this.suggestionsTarget.innerHTML = "";
+      const url = `https://api.thedogapi.com/v1/breeds/search?q=${breed}`
+      console.log(url);
+      fetch(url, {
+        method: "GET",
+        headers: {
+                  "Accept": "application/json",
+                  "X-Api-Key": "f60c3da9-a49e-46f9-8f9c-697e82b80e58"
+                }
+      })
+        .then(response => response.json())
+        .then((data) => {
+          data.forEach((breed) => {
+            this.suggestionsTarget.insertAdjacentHTML('beforeend',
+            `<option value="${breed.name}">`)
+          });
+        });
+    }
   }
 }
