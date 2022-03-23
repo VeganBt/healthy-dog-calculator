01ae8d01200508ef26268967b9af4091539bc7a5
Ofer Peled <ofer2406@gmail.com>
2022-03-23 12:14:38 +0100

    added calculations for dog BMI

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -36,13 +36,47 @@ export default class extends Controller {
 
   showResults(event) {
     event.preventDefault();
-    let height = new FormData(this.formTarget).get('height');
-    // check if height is in cm instead of m
-    if (height > 2) { height = height / 100 }
-    const weight = new FormData(this.formTarget).get('weight');
-    const dogBMI = weight / (height ** 2)
-    console.log(dogBMI);
+    const dogBMI = this.calcDogBMI();
+    this.fetchDog(this.getDogBreed())
+    .then((data) => {
+      // first calculate avg dog BMI
+      const healthyDogBMI = this.getBreedBMI(data);
+      // then compare it with the input dog BMI
+      if (dogBMI < healthyDogBMI.min) {
+        console.log("Dog is underweight");
+      } else if (dogBMI > healthyDogBMI.max) {
+        console.log("Dog is overwight");
+      } else {
+        console.log("dog is healthy");
+      }
+    });
   }
 
+  async fetchDog(breed) {
+    const url = `https://api.thedogapi.com/v1/breeds/search?q=${breed}`
+    let response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "X-Api-Key": "f60c3da9-a49e-46f9-8f9c-697e82b80e58"
+      }
+    })
+    return await response.json();
+  }
 
+  getBreedBMI(data) {
+    const minMaxWeight = data[0].weight.metric.split(' - ');
+    const minMaxHeight = data[0].height.metric.split(' - ');
+    const minHealthyBMI = (parseInt(minMaxWeight[0], 10) / ((parseInt(minMaxHeight[0], 10)) / 100) ** 2);
+    const maxHealthyBMI = (parseInt(minMaxWeight[1], 10) / ((parseInt(minMaxHeight[1], 10)) / 100) ** 2);
+    return {min: minHealthyBMI, max: maxHealthyBMI}
+  }
+
+  calcDogBMI() {
+    // check if height is in cm instead of m
+    let height = new FormData(this.formTarget).get('height');
+    if (height > 2) { height = height / 100 }
+    const weight = new FormData(this.formTarget).get('weight');
+    return weight / (height ** 2)
+  }
 }
