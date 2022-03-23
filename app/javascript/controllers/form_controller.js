1feaae60786f07109f016a70f084299ad77bc8e1
Ofer Peled <ofer2406@gmail.com>
2022-03-23 11:03:00 +0100

    refactor JS a little bit

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -4,28 +4,45 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "results", "form", 'submit', 'suggestions' ]
 
+  getDogBreed() {
+     return (new FormData(this.formTarget).get("breed"));
+  }
+
   getDogInfo(event) {
     event.preventDefault();
     this.submitTarget.disabled = false;
-    const breed = new FormData(this.formTarget).get("breed");
+    const breed = this.getDogBreed();
     if (breed.length > 2) {
       this.suggestionsTarget.innerHTML = "";
       const url = `https://api.thedogapi.com/v1/breeds/search?q=${breed}`
-      console.log(url);
       fetch(url, {
         method: "GET",
         headers: {
-                  "Accept": "application/json",
-                  "X-Api-Key": "f60c3da9-a49e-46f9-8f9c-697e82b80e58"
-                }
-      })
+          "Accept": "application/json",
+          "X-Api-Key": "f60c3da9-a49e-46f9-8f9c-697e82b80e58"
+        }
+        })
         .then(response => response.json())
         .then((data) => {
-          data.forEach((breed) => {
+          data.forEach((breedItem) => {
             this.suggestionsTarget.insertAdjacentHTML('beforeend',
-            `<option value="${breed.name}">`)
-          });
-        });
+              `<option value="${breedItem.name}">`)
+          })
+         });
     }
+
   }
+
+
+  showResults(event) {
+    event.preventDefault();
+    let height = new FormData(this.formTarget).get('height');
+    // check if height is in cm instead of m
+    if (height > 2) { height = height / 100 }
+    const weight = new FormData(this.formTarget).get('weight');
+    const dogBMI = weight / (height ** 2)
+    console.log(dogBMI);
+  }
+
+
 }
